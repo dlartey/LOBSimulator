7a097ac2c7213496fd709cd6990d2f8578cdd9a2
Daniel Lartey <daniel.lartey@outlook.com>
2024-03-06 20:43:36 +0000

    Created a thread to run the generator using the .pt file
    Signed-off-by: Daniel Lartey <daniel.lartey@outlook.com>

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@
 #include <csignal>
 #include <csignal>
 #include <mutex>
-#include <string>        // For std::stoi and std::stod
-#include "OrderBook.hpp" // Include your OrderBook definition
+#include <string>
+#include "OrderBook.hpp"
 #include "DBHandler.hpp"
 #include "API.hpp"
 #include "OrderBookWidget.hpp"
@@ -20,6 +20,7 @@
 
 OrderBook globalOrderBook;
 volatile std::sig_atomic_t gSignalStatus;
+bool flag = false;
 
 // Signal handler function
 void signal_handler(int signal)
@@ -41,20 +42,28 @@ void asyncFunction(std::string baseSQLStatement, DBHandler *handler)
     }
 }
 
-void getNextCentre(float *values, torch::Tensor *output)
+void getNextCentre(float *values, torch::Tensor &output)
 {
-    float *data = (*output).data_ptr<float>();
-    for (int i = 2; i < 9; i++)
-    {
-        // if centre
-        if (data[i] < 0 && data[i + 1] > 0)
-        {
-            values[0] = data[i - 2];
-            values[1] = data[i - 1];
-            values[2] = data[i];
-            values[3] = data[i + 1];
-            values[4] = data[i + 2];
-            values[5] = data[i + 3];
+    if (!output.defined() || output.numel() < 12 || !output.is_contiguous()) {
+        std::cerr << "Output tensor is not valid or does not have the expected number of elements." << std::endl;
+        return;
+    }
+
+    float temp[12];
+    if (output.dim() == 2) {
+        for (int64_t j = 0; j < output.size(1); ++j) {
+            temp[j] = output[0][j].item<float>();
+        }
+        std::cout << std::endl; // New line after each row
+    } else {
+        std::cout << "Tensor is not 2D. It has " << output.dim() << " dimensions." << std::endl;
+    }
+
+    for (int i = 2; i <= 6; ++i) {
+        if (temp[i] < 0 && temp[i + 1] > 0) {
+            for (int j = 0; j < 6; ++j) {
+                values[j] = temp[i - 2 + j];
+            }
             break;
         }
     }
@@ -76,86 +85,61 @@ void startServerWrapper() {
     API::startServer(globalOrderBook);
 }
 
-//int main(int argc, char *argv[])
-//{
-//    // Register signal handler for graceful shutdown
-//    std::signal(SIGINT, signal_handler);
-//    std::thread serverThread(startServerWrapper);
-//    DBHandler handler(getProjectSourceDirectory());
-//    // Start the async SQL test in a separate thread
-//    asyncSQLTest("SELECT * FROM book", &handler);
-//    QApplication app(argc, argv);
-//    OrderBookWidget obw(&handler, &globalOrderBook);
-//    obw.show();
-////    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-//    int result = app.exec();
-//    serverThread.join();
-//
-//    std::cout << "Application exiting..." << std::endl;
-//    return result;
-//}
-
-int main(int argc, char *argv[])
-{
-    /*
-    // Register signal handler for graceful shutdown
-    std::signal(SIGINT, signal_handler);
-
-    DBHandler handler(getProjectSourceDirectory());
-
-    // Start the async SQL test in a separate thread
-    asyncSQLTest("SELECT * FROM book", &handler);
-    QApplication app(argc, argv);
-    OrderBookWidget obw(&handler, &globalOrderBook);
-    obw.show();
-    //    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    int result = app.exec();
-
-    std::cout << "Application exiting..." << std::endl;
-    */
-
-
-    try
-    {
-        std::string currFilePath = __FILE__;
-        std::filesystem::path fullPath(currFilePath);
-        std::string model_path = fullPath.parent_path().string();
-        model_path += "/BatchSize64Base1000Epochs.pt";
+// TODO: Be able to pass the filename of the .pt file
+void generateQuantity() {
+    try {
+        std::string model_path = getProjectSourceDirectory() + "/Test1.pt";
         std::cout << model_path << std::endl;
         torch::jit::script::Module model = torch::jit::load(model_path);
         model.eval();
-        float values[6];
-        torch::Tensor output;
-        for (int i = 0; i < 70000; i++)
-        {
-            if (i == 0)
-            {
-                values[0] = -0.7787362;
-                values[1] = -0.8905415;
-                values[2] = -0.6589257;
-                values[3] = 0.62962157;
-                values[4] = 1.2157797;
-                values[5] = 2.0017693;
+
+        torch::Tensor output = torch::empty({0});
+        float values[6] = {-0.7787362, -0.8905415, -0.6589257, 0.62962157, 1.2157797, 2.0017693};
+
+        while (true) {
+            if (flag) {
+                getNextCentre(values, output);
             }
-            else
-                getNextCentre(values, &output);
+
+            flag = true;
             torch::Tensor S_t = torch::from_blob(values, torch::IntArrayRef{1, 6});
             torch::Tensor z_t = torch::randn({1, 12});
             output = model.forward({z_t, S_t}).toTensor();
 
-            if (output.dim() == 1) {
-                for (int64_t i = 0; i < output.size(0); ++i) {
-                    std::cout << output[i].item<float>() << " ";
+            if (output.dim() == 2) {
+                for (int64_t j = 0; j < output.size(1); ++j) {
+                    std::cout << output[0][j].item<float>() << " ";
                 }
-                std::cout << std::endl;
+                std::cout << std::endl; // New line after each row
             } else {
-                std::cout << "Tensor is not 1D. It has " << output.dim() << " dimensions." << std::endl;
+                std::cout << "Tensor is not 2D. It has " << output.dim() << " dimensions." << std::endl;
             }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
     }
     catch (const c10::Error &e)
     {
         std::cerr << "Error loading the model: " << e.what() << std::endl;
     }
-    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // Register signal handler for graceful shutdown
+    std::signal(SIGINT, signal_handler);
+    std::thread serverThread(startServerWrapper);
+    std::thread startGenerate(generateQuantity);
+    DBHandler handler(getProjectSourceDirectory());
+    // Start the async SQL test in a separate thread
+    asyncSQLTest("SELECT * FROM book", &handler);
+    QApplication app(argc, argv);
+    OrderBookWidget obw(&handler, &globalOrderBook);
+    obw.show();
+//    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    int result = app.exec();
+    serverThread.join();
+    startGenerate.join();
+
+    std::cout << "Application exiting..." << std::endl;
+    return result;
 }
